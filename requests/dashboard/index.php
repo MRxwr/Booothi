9c7dea197073bff672df261befaa29a165eb7a55
MRxwr <nasserhatab@gmail.com>
2026-04-01 00:55:38 +0300

    Update index.php

--- requests/dashboard/index.php
+++ requests/dashboard/index.php
@@ -4,10 +4,10 @@ require_once("../../admin/includes/config.php");
 require_once("../../admin/includes/functions.php");
 
 // check user token \\
-if( checkToken() !== true ){
+if( !checkToken() ){
 	echo outputError(array("msg" => "Unauthorized token"));die();
 }
-
+die();
 // get viewed page from pages folder \\
 if( isset($_GET["a"]) && searchFile("views","api{$_GET["a"]}.php") ){
 	require_once("views/".searchFile("views","api{$_GET["a"]}.php"));
