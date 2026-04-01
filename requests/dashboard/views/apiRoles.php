7132002c9a2052561f0c48d519a7a871a19beff3
MRxwr <nasserhatab@gmail.com>
2026-04-02 00:33:17 +0300

    Update apiRoles.php

--- requests/dashboard/views/apiRoles.php
+++ requests/dashboard/views/apiRoles.php
@@ -100,7 +100,7 @@ switch ($action) {
 
     case "getPermissions":
         // Get list of all possible pages/permissions
-        $pages = selectDB2("id, enTitle, arTitle", "pages", "status = '0' AND hidden = '1' ORDER BY enTitle ASC");
+        $pages = selectDB2("id, enTitle, arTitle", "pages", "status = '0' AND hidden = '1' ORDER BY rank ASC");
         echo outputData($pages ?: []); die();
         break;
 
