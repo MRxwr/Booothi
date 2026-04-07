21175923e76a5c45ad89e96be9f4565f904b9cd6
MRxwr <nasserhatab@gmail.com>
2026-04-07 04:23:14 +0300

    Update apiUser.php

--- requests/dashboard/views/apiUser.php
+++ requests/dashboard/views/apiUser.php
@@ -46,7 +46,7 @@ if( !isset($_REQUEST["action"]) || empty($_REQUEST["action"]) ){
                 $employeeToken = generateToken();
                 updateDB("employees", ["keepMeAlive" => $employeeToken], "id = '{$employee[0]["id"]}'");
                 logStoreActivity("Login", "Employee logged in: " . $employee[0]["fullName"]);
-                die(" test 2");
+                die(" test 3");
                 deleteDB("otp_codes", "id = '{$otp[0]["id"]}'");
                 echo outputData(["msg" => "OTP verified successfully", "token" => $employeeToken, "isRegister" => false, "isStore" => false]);die();
             }else{
