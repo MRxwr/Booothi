969859d65122a5c4e233dddb4cc04939c917e005
MRxwr <nasserhatab@gmail.com>
2026-04-26 03:55:10 +0300

    Update cart.php

--- templates/modals/cart.php
+++ templates/modals/cart.php
@@ -46,7 +46,7 @@
 							echo " ";
 							echo direction($attribute[0]["enTitle"],$attribute[0]["arTitle"]);
 							echo " ";
-							$items = ( !is_null($cart[$i]["collections"])) ? json_decode($cart[$i]["collections"],true) : [];
+							$items = ( is_null($cart[$i]["collections"])) ? [] : json_decode($cart[$i]["collections"],true);
 							for( $y = 0; $y < sizeof($items) ; $y++ ){
 								if ( !empty($items[$y]) ){
 									$productsInfo = selectDB('products', "`id` = '{$items[$y]}'");
